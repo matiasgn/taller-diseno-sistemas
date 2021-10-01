c5cd57048fe13eb314fd58549f91dd30276b4d52
matias <matiasguerrero.n@hotmail.com>
2021-09-30 21:22:20 -0300

    implementacion login laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/menu', 'HomeController@menu')->name('home');
